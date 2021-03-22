939c55087053d66d0cfe132b187490127d974d74
drvilclash <57842677+drvilclash@users.noreply.github.com>
2021-03-22 21:12:04 +0300

    Update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -7,6 +7,8 @@ declare class HydraApi {
 
     editProjectInfo(name: string, avatar: string, group_id: number): any;
 
+    getBalance(userId: number): object;
+    
     sendPayment(toId: number, amount: number): object;
 
     getPaymentLink(): string;
